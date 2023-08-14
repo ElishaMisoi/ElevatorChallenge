492b939b77b18070b7f59186417ff6d0a2008a52
Elisha Misoi <45200891+ElishaMisoi@users.noreply.github.com>
2023-08-14 21:33:24 +0300

    Refactor IElevatorService

--- Application/Services/IElevatorService.cs
+++ Application/Services/IElevatorService.cs
@@ -1,9 +1,20 @@
-﻿namespace ElevatorChallenge.Application.Services
+﻿using ElevatorChallenge.Models;
+
+namespace ElevatorChallenge.Application.Services
 {
     public interface IElevatorService
     {
-        void Initialize(int numberOfElevators, int numberOfFloors, int elevatorMaxCapacity);
-        Task CallElevator(int floor, int passengers);
-        void DisplayElevatorStatus();
+        void Initialize(int numberOfElevators, int numberOfFloors, int maxCapacity = 10);
+        Elevator? RequestNearestElevator(int floorNumber);
+        void MoveElevatorToFloor(int floorNumber, int destinationFloor);
+        void AddFloor(Floor floor);
+        void RemoveFloor(Floor floor);
+        void AddPeopleToFloor(int floor, int peopleToAdd);
+        void RemovePeopleFromFloor(int floor, int numberOfPeople);
+        void AddElevator(Elevator elevator);
+        void RemoveElevator(Elevator elevator);
+        void LoadPeopleIntoElevator(int elevator, int numberOfPeople);
+        void UnLoadPeopleOutOfElevator(int elevator, int numberOfPeople);
+        void GetElevatorStatuses();
     }
 }
