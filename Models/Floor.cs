8b32a200692b5a6b133c0b9a64dff6cfc777a468
Elisha Misoi <45200891+ElishaMisoi@users.noreply.github.com>
2023-08-14 21:34:03 +0300

    Add Floor

---
+++ Models/Floor.cs
@@ -0,0 +1,33 @@
+﻿namespace ElevatorChallenge.Models
+{
+    public class Floor
+    {
+        public int FloorNumber { get; private set; }
+        public int PeopleWaiting { get; private set; }
+
+        public Floor(int floorNumber)
+        {
+            FloorNumber = floorNumber;
+            PeopleWaiting = 0;
+        }
+
+        public void AddPeople(int numberOfPeople)
+        {
+            PeopleWaiting += numberOfPeople;
+            Console.WriteLine($"{numberOfPeople} people added to Floor {FloorNumber}.");
+        }
+
+        public void RemovePeople(int numberOfPeople)
+        {
+            if (PeopleWaiting >= numberOfPeople)
+            {
+                PeopleWaiting -= numberOfPeople;
+                Console.WriteLine($"{numberOfPeople} people removed from Floor {FloorNumber}.");
+            }
+            else
+            {
+                Console.WriteLine("Cannot remove more people than currently waiting.");
+            }
+        }
+    }
+}
