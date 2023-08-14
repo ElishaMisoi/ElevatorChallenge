e20fdb11b517446a11111759ac7f2ae2bc5b9361
Elisha Misoi <45200891+ElishaMisoi@users.noreply.github.com>
2023-08-14 11:19:57 +0300

    Create IElevatorService Interface

---
+++ Application/Services/IElevatorService.cs
@@ -0,0 +1,9 @@
+﻿namespace ElevatorChallenge.Application.Services
+{
+    public interface IElevatorService
+    {
+        void Initialize(int numberOfElevators, int numberOfFloors, int elevatorMaxCapacity);
+        Task CallElevator(int floor, int passengers);
+        void DisplayElevatorStatus();
+    }
+}
