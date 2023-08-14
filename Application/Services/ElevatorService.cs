cad90510e368ab5fc2d4e798d394b50d828354c8
Elisha Misoi <45200891+ElishaMisoi@users.noreply.github.com>
2023-08-14 21:33:15 +0300

    Refactor ElevatorService

--- Application/Services/ElevatorService.cs
+++ Application/Services/ElevatorService.cs
@@ -1,4 +1,5 @@
-﻿using ElevatorChallenge.Common.Exceptions;
+﻿using ElevatorChallenge.Common.Enums;
+using ElevatorChallenge.Common.Exceptions;
 using ElevatorChallenge.Models;
 
 namespace ElevatorChallenge.Application.Services
@@ -6,133 +7,210 @@ namespace ElevatorChallenge.Application.Services
     public class ElevatorService : IElevatorService
     {
         private List<Elevator>? _elevators;
+        private List<Floor>? _floors;
         private bool _isInitialized;
-        private int _totalPassengersTransported;
 
         public ElevatorService()
         {
             _isInitialized = false;
-            _totalPassengersTransported = 0;
         }
 
-        public void Initialize(int numberOfElevators, int numberOfFloors, int elevatorMaxCapacity)
+        public void Initialize(int numberOfElevators, int numberOfFloors, int maxCapacity = 10)
         {
+            if (numberOfElevators < 1)
+                throw new Exception("At least one elevator is required");
+
+            if (numberOfFloors < 2)
+                throw new Exception("At least two floors are required");
+
             _elevators = new List<Elevator>();
 
             for (int i = 0; i < numberOfElevators; i++)
             {
-                _elevators.Add(new Elevator(i, elevatorMaxCapacity));
+                _elevators.Add(new Elevator(elevatorNumber: (i + 1), capacity: maxCapacity));
+            }
+
+            _floors = new List<Floor>();
+
+            for (int i = 0; i < numberOfFloors; i++)
+            {
+                _floors.Add(new Floor(floorNumber: (i + 1)));
             }
 
             _isInitialized = true;
         }
 
-        public async Task CallElevator(int floor, int totalPassengers)
+        public Elevator? RequestNearestElevator(int floorNumber)
         {
             if (!_isInitialized)
                 throw new InitializationException("Elevator Service not initialized");
 
-            //if (_elevators != null && _elevators.Any())
-            //{
-            //    List<Task> elevatorTasks = new List<Task>();
-
-            //    foreach (var elevator in _elevators)
-            //    {
-            //        if (elevator.IsAvailable())
-            //        {
-            //            Task elevatorTask = Task.Run(() =>
-            //            {
-            //                elevator.MoveToFloor(floor);
-            //                elevator.LoadPassenger(passengers);
-            //            });
-            //            elevatorTasks.Add(elevatorTask);
-            //        }
-            //    }
-
-            //    await Task.WhenAll(elevatorTasks);
-            //}
+            // Find the nearest available elevator
+            Elevator? nearestElevator = null;
+            int minDistance = int.MaxValue;
 
             if (_elevators != null && _elevators.Any())
             {
-                int passengersPerElevator = totalPassengers / _elevators.Count; // Divide passengers evenly
-                int remainingPassengers = totalPassengers % _elevators.Count; // Distribute remaining passengers
-
-                List<Task> elevatorTasks = new List<Task>();
-                object lockObject = new object(); // Create a lock object
-
                 foreach (var elevator in _elevators)
                 {
-                    if (elevator.IsAvailable())
+                    if (elevator.Direction == Direction.Stationary)
                     {
-                        int passengersToLoad = passengersPerElevator;
-
-                        if (remainingPassengers > 0)
+                        int distance = Math.Abs(elevator.CurrentFloor - floorNumber);
+                        if (distance < minDistance)
                         {
-                            passengersToLoad++;
-                            remainingPassengers--;
+                            minDistance = distance;
+                            nearestElevator = elevator;
                         }
-
-                        Task elevatorTask = Task.Run(() =>
-                        {
-                            elevator.MoveToFloor(floor);
-                            elevator.LoadPassenger(passengersToLoad);
-
-                            // Lock to update _totalPassengersTransported
-                            lock (lockObject)
-                            {
-                                _totalPassengersTransported += passengersToLoad; // Update total passengers transported
-                                Console.WriteLine($"Total passengers transported: {_totalPassengersTransported}");
-                            }
-                        });
-
-                        elevatorTasks.Add(elevatorTask);
                     }
                 }
 
-                await Task.WhenAll(elevatorTasks);
-            }
-        }
-
-        private Elevator? FindNearestAvailableElevator(int floor)
-        {
-            if (_elevators != null && _elevators.Any())
-            {
-                lock (_elevators) // Lock access to the elevators list
+                if (nearestElevator != null)
                 {
-                    Elevator? nearestElevator = null;
-                    int minDistance = int.MaxValue;
-
-                    foreach (Elevator elevator in _elevators)
-                    {
-                        if (elevator.IsAvailable())
-                        {
-                            int distance = Math.Abs(elevator.CurrentFloor - floor);
-                            if (distance < minDistance)
-                            {
-                                minDistance = distance;
-                                nearestElevator = elevator;
-                            }
-                        }
-                    }
-
+                    nearestElevator.MoveToFloor(floorNumber);
+                    Console.WriteLine($"Elevator {nearestElevator.ElevatorNumber} has arrived at floor {floorNumber}");
                     return nearestElevator;
+                }
+                else
+                {
+                    Console.WriteLine("No available elevators at the moment.");
+                    return null;
                 }
             }
 
             return null;
         }
 
-        public void DisplayElevatorStatus()
+        public void MoveElevatorToFloor(int floorNumber, int destinationFloor)
         {
-            Console.WriteLine("Elevator Status:");
+            Elevator? nearestElevator = RequestNearestElevator(floorNumber);
 
-            if (_elevators != null && _elevators.Any())
+            if (nearestElevator != null)
             {
-                foreach (var elevator in _elevators)
-                {
-                    Console.WriteLine($"Elevator - Current Floor: {elevator.CurrentFloor}, Direction: {elevator.CurrentDirection}, Capacity: {elevator.CurrentCapacity}/{elevator.MaxCapacity}");
-                }
+                nearestElevator.MoveToFloor(destinationFloor);
+                Console.WriteLine($"Elevator {nearestElevator.ElevatorNumber} has arrived at floor {floorNumber}");
             }
+        }
+
+        public void AddFloor(Floor floor)
+        {
+            if (!_isInitialized)
+                ThrowNotInitialized();
+
+            _floors!.Add(floor);
+        }
+
+        public void RemoveFloor(Floor floor)
+        {
+            if (!_isInitialized)
+                ThrowNotInitialized();
+
+            bool removed = _floors!.Remove(floor);
+
+            if (!removed)
+            {
+                Console.WriteLine("The floor does not exist");
+            }
+        }
+
+        public void AddPeopleToFloor(int floor, int numberOfPeople)
+        {
+            if (!_isInitialized)
+                ThrowNotInitialized();
+
+            Floor? existingFloor = _floors!.FirstOrDefault(s => s.FloorNumber == floor);
+
+            if (existingFloor == null)
+            {
+                Console.WriteLine("The floor does not exist");
+                return;
+            }
+
+            existingFloor.AddPeople(numberOfPeople);
+        }
+
+        public void RemovePeopleFromFloor(int floor, int numberOfPeople)
+        {
+            if (!_isInitialized)
+                ThrowNotInitialized();
+
+            Floor? existingFloor = _floors!.FirstOrDefault(s => s.FloorNumber == floor);
+
+            if (existingFloor == null)
+            {
+                Console.WriteLine("The floor does not exist");
+                return;
+            }
+
+            existingFloor.RemovePeople(numberOfPeople);
+        }
+
+        public void AddElevator(Elevator elevator)
+        {
+            if (!_isInitialized)
+                ThrowNotInitialized();
+
+            _elevators!.Add(elevator);
+        }
+
+        public void RemoveElevator(Elevator elevator)
+        {
+            if (!_isInitialized)
+                ThrowNotInitialized();
+
+            bool removed = _elevators!.Remove(elevator);
+
+            if (!removed)
+            {
+                Console.WriteLine("The elevator does not exist");
+            }
+        }
+
+        public void LoadPeopleIntoElevator(int elevator, int numberOfPeople)
+        {
+            if (!_isInitialized)
+                ThrowNotInitialized();
+
+            Elevator? existingElevator = _elevators!.FirstOrDefault(s => s.ElevatorNumber == elevator);
+
+            if (existingElevator == null)
+            {
+                Console.WriteLine("The elevator does not exist");
+                return;
+            }
+
+            existingElevator.LoadPeople(numberOfPeople);
+        }
+
+        public void UnLoadPeopleOutOfElevator(int elevator, int numberOfPeople)
+        {
+            if (!_isInitialized)
+                ThrowNotInitialized();
+
+            Elevator? existingElevator = _elevators!.FirstOrDefault(s => s.ElevatorNumber == elevator);
+
+            if (existingElevator == null)
+            {
+                Console.WriteLine("The elevator does not exist");
+                return;
+            }
+
+            existingElevator.UnloadPeople(numberOfPeople);
+        }
+
+        public void GetElevatorStatuses()
+        {
+            if (!_isInitialized)
+                ThrowNotInitialized();
+
+            foreach (var elevator in _elevators!)
+            {
+                elevator.UpdateStatus();
+            }
+        }
+
+        private void ThrowNotInitialized()
+        {
+            throw new InitializationException("Elevator Service not initialized");
         }
     }
 }
