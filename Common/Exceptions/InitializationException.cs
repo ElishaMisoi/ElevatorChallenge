aa11b5ca7a88c47c305c215869a8a00872f26f83
Elisha Misoi <45200891+ElishaMisoi@users.noreply.github.com>
2023-08-14 11:20:22 +0300

    Create InitializationException

---
+++ Common/Exceptions/InitializationException.cs
@@ -0,0 +1,18 @@
+﻿namespace ElevatorChallenge.Common.Exceptions
+{
+    public class InitializationException : Exception
+    {
+        public InitializationException() : base()
+        {
+        }
+
+        public InitializationException(string message) : base(message)
+        {
+        }
+
+        public InitializationException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+    }
+
+}
