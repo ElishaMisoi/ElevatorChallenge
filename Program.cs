dad399878742e73c8b82b7f94100dc12d7073ea4
Elisha Misoi <45200891+ElishaMisoi@users.noreply.github.com>
2023-08-14 11:21:27 +0300

    Update Program

--- Program.cs
+++ Program.cs
@@ -1,11 +1,37 @@
 ﻿
+using ElevatorChallenge.Application;
+using ElevatorChallenge.Application.Services;
+using Microsoft.Extensions.DependencyInjection;
+
 namespace ElevatorChallenge
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            Console.WriteLine("Elecator Challange");
+            // Create a service collection
+            var services = new ServiceCollection();
+
+            // Register dependencies
+            services.AddTransient<IElevatorService, ElevatorService>();
+
+            // Build the service provider
+            var serviceProvider = services.BuildServiceProvider();
+
+            // Resolve and use dependencies
+            IElevatorService elevatorService = serviceProvider.GetRequiredService<IElevatorService>();
+
+            // Initialize Elevator App
+            ElevatorApp elevatorApp = new ElevatorApp(elevatorService);
+
+            // Run
+            await elevatorApp.Run(elevatorMaxCapacity: 8);
+
+            // Dispose serviceProvider when done
+            if (serviceProvider is IDisposable disposable)
+            {
+                disposable.Dispose();
+            }
         }
     }
 }
